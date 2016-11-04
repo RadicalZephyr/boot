39c6b1886b961875762757ac14e436eb52bf97ae
Geoff Shannon <geoffpshannon@gmail.com>
2016-11-04 15:35:59 -0500

    Start characterizing current task-options! behavior

--- boot/core/test/boot/core_test.clj
+++ boot/core/test/boot/core_test.clj
@@ -2,6 +2,30 @@
   (:require [clojure.test :refer :all]
             [boot.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def test-task)
+
+(deftest task-options!-test
+  (testing "set default task options"
+    (with-redefs [test-task (fn [& xs] xs)]
+      (is (= nil (test-task)))
+
+      (task-options! test-task {:foo :bar})
+      (is (= [:foo :bar] (test-task)))))
+
+  (testing "override default task options"
+    (with-redefs [test-task (fn [& xs] xs)]
+      (is (= nil (test-task)))
+
+      (task-options! test-task {:foo :bar})
+      (is (= [:foo :bar] (test-task)))
+
+      (task-options! test-task {:foo :frobozz})
+      (is (= [:foo :bar :foo :frobozz] (test-task)))))
+
+  (testing "update options with a function"
+    (with-redefs [test-task (fn [& xs] xs)]
+      (task-options! test-task {:foo :bar})
+      (is (= [:foo :bar] (test-task)))
+
+      (task-options! test-task #(assoc % :baz 1))
+      (is (= [:foo :bar :foo :bar :baz 1] (test-task))))))
